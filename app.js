e5af62ab47df3a241e7efd7f4eeb6a05630494a9
raj <rajraman21211@gmail.com>
2024-07-27 19:49:37 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { Leetcode } = require('@codingsnack/leetcode-api');
 const cheerio = require('cheerio');
+const cors = require('cors');  // Import the cors package
 require('dotenv').config()
 
 const app = express();
 const port = 4000;
+
+// Enable CORS for all routes
+app.use(cors());
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
